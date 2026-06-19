7ff34bc6de86539f065fde0a83296f11b41f0735
Hugo Marotta <humtta@proton.me>
2026-06-19 12:58:57 -0300

    Add client.NewClient function
    NewClient returns a Client with a default timeout and base URL.

--- internal/client/client.go
+++ internal/client/client.go
@@ -2,9 +2,24 @@ package client
 
 import (
 	"net/http"
+	"time"
+)
+
+const (
+	baseURL = "https://rosalind.info"
+	timeout = 10 * time.Second
 )
 
 type Client struct {
 	httpClient *http.Client
 	baseURL    string
+}
+
+func NewClient() *Client {
+	return &Client{
+		httpClient: &http.Client{
+			Timeout: timeout,
+		},
+		baseURL: baseURL,
+	}
 }
