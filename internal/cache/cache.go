17d77b2328a7e292e726514d816d54c4c38fa0bb
Hugo Marotta <humtta@proton.me>
2026-07-19 14:05:45 -0300

    Add cache.cachePath function
    cachePath returns the path to the cache file.

--- internal/cache/cache.go
+++ internal/cache/cache.go
@@ -1,10 +1,23 @@
 package cache
 
 import (
+	"fmt"
+	"os"
+	"path/filepath"
 	"time"
 )
+
+const cacheSubpath = "/rosalind-cli/cache.json"
 
 type Cache struct {
 	path string
 	ttl  time.Duration
+}
+
+func cachePath() (string, error) {
+	dir, err := os.UserCacheDir()
+	if err != nil {
+		return "", fmt.Errorf("locate user cache dir: %w", err)
+	}
+	return filepath.Join(dir, cacheSubpath), nil
 }
