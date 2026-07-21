dbf91d7dd58caca5d067b0a4588355dff3932333
Hugo Marotta <humtta@proton.me>
2026-07-21 10:26:33 -0300

    Add parser.resolveURL function
    resolveURL joins a relative path to a base URL.

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,5 +1,21 @@
 package parser
 
 import (
+	"net/url"
+
 	_ "github.com/PuerkitoBio/goquery"
 )
+
+func resolveURL(base, path string) (string, error) {
+	b, err := url.Parse(base)
+	if err != nil {
+		return "", err
+	}
+
+	p, err := url.Parse(path)
+	if err != nil {
+		return "", err
+	}
+
+	return b.ResolveReference(p).String(), nil
+}
