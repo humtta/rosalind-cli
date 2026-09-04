4ac3a6854bbb2eb97c455784801c7d93d1df80f7
Hugo Marotta <humtta@proton.me>
2026-09-03 23:39:43 -0300

    Add rosalind.Rosalind struct
    Rosalind provides access to Rosalind problems.

--- internal/rosalind/rosalind.go
+++ internal/rosalind/rosalind.go
@@ -1,2 +1,13 @@
 // Package rosalind provides access to Rosalind problems.
 package rosalind
+
+import (
+	"github.com/humtta/rosalind-cli/internal/cache"
+	"github.com/humtta/rosalind-cli/internal/client"
+)
+
+// Rosalind provides access to Rosalind problems.
+type Rosalind struct {
+	client *client.Client
+	cache  *cache.Cache
+}
